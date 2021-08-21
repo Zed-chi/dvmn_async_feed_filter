d4499c8fd903736b4e6515c4350010c77bdbfccc
zed-chi <zed-chi@mail.ru>
2021-08-21 15:28:14 +0300

    changed time getter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from datetime import datetime
 from enum import Enum
+from time import monotonic
 
 import aiohttp
 import pymorphy2
@@ -45,25 +46,24 @@ class Result:
 
 async def process_article(
     session, morph, url, positive_words, negative_words, results_container
-):
-    address = "Not Loaded"
+):    
     word_count = 0
     positive_rate = None
     negative_rate = None
     status = None
-    time = 0
+    process_time = 0
     try:
         async with timeout(PROCESS_TIME):
-            start = datetime.now()
+            start = monotonic()
             html = await fetch(session, url)
             text = sanitize(html, True)
             words = split_by_words(morph, text)
             positive_rate = calculate_jaundice_rate(words, positive_words)
             negative_rate = calculate_jaundice_rate(words, negative_words)
-            address = url
             word_count = len(words)
-            status = ProcessingStatus.OK.value
-            time = datetime.now() - start            
+            status = ProcessingStatus.OK.value            
+            end_time = monotonic()
+            process_time = round((end_time - start), 2)
     except aiohttp.client_exceptions.ClientResponseError:
         status = ProcessingStatus.FETCH_ERROR.value
     except ArticleNotFound:
@@ -72,7 +72,7 @@ async def process_article(
         status = ProcessingStatus.TIMEOUT.value
     finally:
         result = Result(
-            address, word_count, positive_rate, negative_rate, status, time
+            url, word_count, positive_rate, negative_rate, status, process_time
         )
         results_container.append(result)
 
@@ -88,7 +88,7 @@ async def fetch(session, url):
         return await response.text()
 
 
-async def main():
+async def main(urls=None):   
     morph = pymorphy2.MorphAnalyzer()
     positive_words = get_words_from_file(POSITIVE_PATH)
     negative_words = get_words_from_file(NEGATIVE_PATH)
@@ -97,7 +97,7 @@ async def main():
 
     async with aiohttp.ClientSession() as session:
         async with create_task_group() as tg:
-            for url in TEST_ARTICLES:
+            for url in urls:
                 tg.start_soon(
                     process_article,
                     session,
@@ -117,5 +117,27 @@ async def main():
         print(f"\ttime {i.time}")
 
 
+async def get_articles_results(urls=None):   
+    morph = pymorphy2.MorphAnalyzer()
+    positive_words = get_words_from_file(POSITIVE_PATH)
+    negative_words = get_words_from_file(NEGATIVE_PATH)
+
+    results = []
+
+    async with aiohttp.ClientSession() as session:
+        async with create_task_group() as tg:
+            for url in urls:
+                tg.start_soon(
+                    process_article,
+                    session,
+                    morph,
+                    url,
+                    positive_words,
+                    negative_words,
+                    results,
+                )
+    return results
+
+
 if __name__ == "__main__":
-    run(main)
+    run(main, TEST_ARTICLES)
