422e861ce9b2154bee5f8a59942e7861568ec281
zed-chi <zed-chi@mail.ru>
2021-08-22 23:36:17 +0300

    moved consts to file

---
+++ config.py
@@ -0,0 +1,13 @@
+DEFAULT_TIMEOUT = 8
+NEGATIVE_WORDS_PATH = "./charged_dict/negative_words.txt"
+POSITIVE_WORDS_PATH = "./charged_dict/positive_words.txt"
+TEST_ARTICLES = [
+    "https://inosmi.ru/science/20210821/250351807.html",
+    "https://inosmi.ru/science/20210820/250344124.html",
+    "https://inosmi.ru/science/20210819/250338925.html",
+    "https://inosmi.ru/science/20210817/250325101.html",
+    "https://inosmi.ru/science/20210817/250323544.html",
+    "https://inosmi.ru/economic/20210812/250298533.html",
+    "https://inosmi.ru/science/20210809/250273593.html",
+    "https://testinosmi.zedchi.repl.co",
+]
