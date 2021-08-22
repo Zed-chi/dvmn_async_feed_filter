e2cae73a1eecc3023449f6bdff5428191d951184
zed-chi <zed-chi@mail.ru>
2021-08-22 23:35:05 +0300

    moved load func to file

--- text_tools.py
+++ text_tools.py
@@ -1,6 +1,12 @@
+import asyncio
 import string
 
 import pymorphy2
+
+
+def load_words_from_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as file:
+        return file.read().splitlines()
 
 
 def _clean_word(word):
@@ -38,6 +44,42 @@ def split_by_words(morph, text):
     return words
 
 
+async def a_split_by_words(morph, text):
+    """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
+    words = []
+    for word in text.split():
+        cleaned_word = _clean_word(word)
+        normalized_word = morph.parse(cleaned_word)[0].normal_form
+        if len(normalized_word) > 2 or normalized_word == "не":
+            words.append(normalized_word)
+        await asyncio.sleep(0.1)
+    return words
+
+
+def calculate_jaundice_rate(article_words, charged_words):
+    """Расчитывает желтушность текста, принимает список "заряженных" слов и ищет их внутри article_words."""
+
+    if not article_words:
+        return 0.0
+
+    found_charged_words = [word for word in article_words if word in set(charged_words)]
+
+    score = len(found_charged_words) / len(article_words) * 100
+
+    return round(score, 2)
+
+
+def test_calculate_jaundice_rate():
+    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
+    assert (
+        33.0
+        < calculate_jaundice_rate(
+            ["все", "аутсайдер", "побег"], ["аутсайдер", "банкротство"]
+        )
+        < 34.0
+    )
+
+
 def test_split_by_words():
     # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
     # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
@@ -55,29 +97,3 @@ def test_split_by_words():
         "стать",
         "начало",
     ]
-
-
-def calculate_jaundice_rate(article_words, charged_words):
-    """Расчитывает желтушность текста, принимает список "заряженных" слов и ищет их внутри article_words."""
-
-    if not article_words:
-        return 0.0
-
-    found_charged_words = [
-        word for word in article_words if word in set(charged_words)
-    ]
-
-    score = len(found_charged_words) / len(article_words) * 100
-
-    return round(score, 2)
-
-
-def test_calculate_jaundice_rate():
-    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
-    assert (
-        33.0
-        < calculate_jaundice_rate(
-            ["все", "аутсайдер", "побег"], ["аутсайдер", "банкротство"]
-        )
-        < 34.0
-    )
