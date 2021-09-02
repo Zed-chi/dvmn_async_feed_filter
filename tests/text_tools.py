8bf8b2391e2481f6a85b5693ea296ceec2ea271f
zed-chi <zed-chi@mail.ru>
2021-09-02 21:20:58 +0300

    fixed

--- tests/text_tools.py
+++ tests/text_tools.py
@@ -1,26 +1,35 @@
-def test_calculate_jaundice_rate():
-    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
+import pymorphy2
+import pytest
+from text_tools import calculate_jaundice_rate, split_by_words
+
+
+@pytest.mark.asyncio
+async def test_calculate_jaundice_rate():
+    assert -0.01 < await calculate_jaundice_rate([], []) < 0.01
     assert (
         33.0
-        < calculate_jaundice_rate(
+        < await calculate_jaundice_rate(
             ["все", "аутсайдер", "побег"], ["аутсайдер", "банкротство"]
         )
         < 34.0
     )
 
 
-def test_split_by_words():
+@pytest.mark.asyncio
+async def test_split_by_words():
     # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
     # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
     morph = pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(morph, "Во-первых, он хочет, чтобы") == [
+    assert await split_by_words(morph, "Во-первых, он хочет, чтобы") == [
         "во-первых",
         "хотеть",
         "чтобы",
     ]
 
-    assert split_by_words(morph, "«Удивительно, но это стало началом!»") == [
+    assert await split_by_words(
+        morph, "«Удивительно, но это стало началом!»"
+    ) == [
         "удивительно",
         "это",
         "стать",
